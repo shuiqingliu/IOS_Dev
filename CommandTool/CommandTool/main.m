c2a5154ab39dd10b57e91aedafe692089f08fe05
qingliu <shuiqingliu14@gmail.com>
2017-10-07 09:42:56 +0800

    add:demo of command tool

---
+++ CommandTool/CommandTool/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CommandTool
+//
+//  Created by qingliu on 9/29/17.
+//  Copyright © 2017 qingliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello World!\n");
+    return 0;
+}
