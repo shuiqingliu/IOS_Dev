c6465ea69bd89148afb12024a55036d39fd4d94e
qingliu <shuiqingliu14@gmail.com>
2017-10-09 08:21:26 +0800

    init:creat project

---
+++ CurrencyConversion/CurrencyConversion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyConversion
+//
+//  Created by qingliu on 10/8/17.
+//  Copyright © 2017 qingliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrencyConversion/CurrencyConversion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConversion
+//
+//  Created by qingliu on 10/8/17.
+//  Copyright © 2017 qingliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
