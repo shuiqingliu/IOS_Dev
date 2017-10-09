11e0357b2b5f5d6c9217b10443175b3e694a71bf
qingliu <shuiqingliu14@gmail.com>
2017-10-09 11:43:22 +0800

    compelte currency convert

--- CurrencyConversion/CurrencyConversion/ViewController.m
+++ CurrencyConversion/CurrencyConversion/ViewController.m
@@ -10,7 +10,8 @@
 #import <CurrencyRequest/CRCurrencyRequest.h>
 #import <CurrencyRequest/CRCurrencyResults.h>
 
-@interface ViewController ()
+@interface ViewController () <CRCurrencyRequestDelegate>
+@property (nonatomic) CRCurrencyRequest *req;
 @property (weak, nonatomic) IBOutlet UITextField *inputField;
 @property (weak, nonatomic) IBOutlet UIButton *convertButton;
 @property (weak, nonatomic) IBOutlet UILabel *currencyEUR;
@@ -22,7 +23,31 @@
 @implementation ViewController
 - (IBAction)tapButton:(id)sender {
     self.convertButton.enabled = NO;
+    self.req = [[CRCurrencyRequest alloc] init];
+    self.req.delegate = self;
+    [self.req start];
+}
+
+- (void)currencyRequest:(CRCurrencyRequest *)req
+    retrievedCurrencies:(CRCurrencyResults *)currencies{
     
+    double inputValue = [self.inputField.text floatValue];
+    //get exchange rate and calculate money
+    double eurCurrency = inputValue * currencies.EUR;
+    double jpyCurrency = inputValue * currencies.JPY;
+    double gbpCurrency = inputValue * currencies.GBP;
+    
+    //double convert to string
+    NSString *eurValue = [NSString stringWithFormat:@"%.2f",eurCurrency];
+    NSString *jpyValue = [NSString stringWithFormat:@"%.2f",jpyCurrency];
+    NSString *gbpValue = [NSString stringWithFormat:@"%.2f",gbpCurrency];
+    
+    //set value to relevant field
+    self.currencyEUR.text = eurValue;
+    self.currencyJPY.text = jpyValue;
+    self.currencyGBP.text = gbpValue;
+    
+    //recovery button status
     self.convertButton.enabled = YES;
 }
 
