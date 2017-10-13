3db7d1e13dc5cffb1663e622c630942bf1ca692d
qingliu <shuiqingliu14@gmail.com>
2017-10-13 20:48:05 +0800

    init assignment1

---
+++ Assignment1/Assignment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment1
+//
+//  Created by qingliu on 10/13/17.
+//  Copyright © 2017 qingliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment1/Assignment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment1
+//
+//  Created by qingliu on 10/13/17.
+//  Copyright © 2017 qingliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
