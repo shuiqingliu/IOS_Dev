4b9f785e084f43663bb21113c29f42d7dfe2e00d
qingliu <shuiqingliu14@gmail.com>
2017-10-15 23:58:31 +0800

    completed assignment1

--- Assignment1/Assignment1/AllterViewController.m
+++ Assignment1/Assignment1/AllterViewController.m
@@ -24,10 +24,50 @@
     alterView.backgroundColor = [UIColor whiteColor];
     alterView.layer.cornerRadius = 5;
     [self.view addSubview:alterView];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissVC:)];
+    [self.view addGestureRecognizer:tap];
+    
 }
 
-- (void)backAction{
-    [self dismissViewControllerAnimated:YES  completion:nil];
+-(void) dismissVC:(id) sender{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+//- (void)loadView{
+//    NSLog(@"第二个 storyboard 加载 view");
+//}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"第二个控制器的 view 将要显示");
+}
+
+- (void)viewWillLayoutSubviews{
+    NSLog(@"第二个控制器的 view 将要布局子控件");
+}
+
+- (void)viewDidLayoutSubviews{
+    NSLog(@"第二个控制器的 view 布局子控件完成");
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"第二个控制器的 view 显示完毕");
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    NSLog(@"第二个控制器的 view 即将消失");
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    NSLog(@"第二个控制器的 view 完全消失");
+}
+
 
 @end
--- Assignment1/Assignment1/ViewController.m
+++ Assignment1/Assignment1/ViewController.m
@@ -19,7 +19,6 @@
 
 @implementation ViewController
 
-
 //set UILable can display mutil lines
 - (void) setHelloLable:(UILabel *)helloLable{
     NSString *screenSize = [NSString stringWithFormat:@"%f",[UIScreen mainScreen].bounds.size.width];
@@ -27,32 +26,21 @@
     helloLable.preferredMaxLayoutWidth = [UIScreen mainScreen].bounds.size.width;
     helloLable.numberOfLines = 0;
 }
-//set border width and corner radius
-//- (void) setTapButton1:(UIButton *)tapButton1{
-//    tapButton1.layer.borderWidth = 1;
-//    tapButton1.layer.cornerRadius = 5;
-//}
-
-//- (void) setTapButton2:(UIButton *)tapButton2{
-//    tapButton2.layer.borderWidth = 1;
-//    tapButton2
-//    //self.tapButton2.currentImage.images = [UIImage imageNamed:@"heart_face"];
-//}
 
 - (IBAction)clickMe:(id)sender {
     //set Lable title
     NSString *clickAgain = [NSString stringWithFormat:@"再点一次!"];
     [self.tapButton1 setTitle:clickAgain forState:(UIControlStateNormal)];
     printf("ClickCompleted");
-    
-
-    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     //get screen width and height
+    _tapButton1.layer.borderWidth = 1;
+    _tapButton1.layer.cornerRadius = 5;
+    
     CGFloat width = [UIScreen mainScreen].bounds.size.width/2;
     CGFloat height = [UIScreen mainScreen].bounds.size.height/2;
     // invoke UIColorFromRGB(defined in .h file ) method to transform hex color to UIColor
@@ -68,16 +56,18 @@
     [self.tapButton2 addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
     //add button to subView
     [self.view addSubview:_tapButton2];
+    NSLog(@"第一个 view 加载 完毕");
 }
 
 //declare button selector
 - (void)buttonClick{
     //creat instance alter view controller
-    AlterViewController *alter = [AlterViewController new];
+    AlterViewController *alter = [[AlterViewController new]init];
     //when button clicked that set background image
     [self.tapButton2 setBackgroundImage:[UIImage imageNamed:@"heart_face"] forState:(UIControlStateNormal)];
     //set presentation modal is Over Current context
     alter.modalPresentationStyle = UIModalPresentationOverCurrentContext;
+    //alter.modalPresentationStyle = UIModalPresentationPopover;
     //add new view
     [self presentViewController:alter animated:YES  completion:nil];
 }
@@ -87,6 +77,41 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+//- (void)loadView{
+//    NSLog(@"第一个 storyboard 加载 view");
+//}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"第一个控制器的 view 将要显示");
+}
+
+- (void)viewWillLayoutSubviews{
+    NSLog(@"第一个控制器的 view 将要布局子控件");
+}
+
+- (void)viewDidLayoutSubviews{
+    NSLog(@"第一个控制器的 view 布局子控件完成");
+}
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"第一个控制器的 view 显示完毕");
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    NSLog(@"第一个控制器的 view 即将消失");
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    NSLog(@"第一个控制器的 view 完全消失");
+}
+
 
 
 @end
