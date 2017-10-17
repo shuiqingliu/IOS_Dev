f1fff09fd6813ba94ffc76a175917e5c2f4f74b2
qingliu <shuiqingliu14@gmail.com>
2017-10-17 22:49:04 +0800

    commpleted calculate distance

--- CalcuDistance/CalcuDistance/ViewController.m
+++ CalcuDistance/CalcuDistance/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <DistanceGetter/DGDistanceRequest.h>
 
 @interface ViewController ()
+
+@property (nonatomic) DGDistanceRequest *requst;
 
 @property (weak, nonatomic) IBOutlet UITextField *startLocation;
 
@@ -26,31 +29,76 @@
 @end
 
 @implementation ViewController
-
-- (void) setStartLocation:(UITextField *)startLocation{
-    startLocation.layer.borderWidth = 1;
-    startLocation.layer.cornerRadius = 5;
-}
-
-- (void) setDestinationA:(UITextField *)destinationA{
-    destinationA.layer.borderWidth = 1;
-    destinationA.layer.cornerRadius = 5;
-}
-
-- (void) setDestinationB:(UITextField *)destinationB{
-    destinationB.layer.borderWidth = 1;
-    destinationB.layer.cornerRadius = 5;
-}
-
-- (void) setDestinationC:(UITextField *)destinationC{
-    destinationC.layer.borderWidth = 1;
-    destinationC.layer.cornerRadius = 5;
-}
-
-
+//button action define
 - (IBAction)calculateButtonTap:(id)sender {
+    
+    self.calculateButton.enabled = NO;
+    //init DGDistanceRequest object
+    self.requst = [DGDistanceRequest alloc];
+    //get string from textfield
+    NSString *start = self.startLocation.text;
+    NSString *destA = self.destinationA.text;
+    NSString *destB = self.destinationB.text;
+    NSString *destC = self.destinationC.text;
+    NSArray *dest = @[destA,destB,destC];
 
+    self.requst = [self.requst initWithLocationDescriptions:dest sourceDescription:start];
+    //weak reference
+    __weak ViewController *weakSelf = self;
+    //invoke callback
+    self.requst.callback = ^(NSArray *distances) {
+        ViewController *strongSelf = weakSelf;
+        if(!strongSelf) return ;
+        
+        NSNull *badResult = [NSNull null];
+        //distance A calculate
+        if(distances[0] != badResult){
+            double num = ([distances[0] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km",num];
+            strongSelf.distanceA.text = x;
+        }
+        else{
+            strongSelf.distanceA.text = @"Error";
+        }
+        //distance B calculate
+        if(distances[1] != badResult){
+            double num = ([distances[1] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km",num];
+            strongSelf.distanceB.text = x;
+        }
+        else{
+            strongSelf.distanceB.text = @"Error";
+        }
+        //distance C calculate
+        if(distances[2] != badResult){
+            double num = ([distances[2] floatValue]/1000.0);
+            NSString *x = [NSString stringWithFormat:@"%.2f km",num];
+            strongSelf.distanceC.text = x;
+        }
+        else{
+            strongSelf.distanceC.text = @"Error";
+        }
+        
+        strongSelf.requst = nil;
+        strongSelf.calculateButton.enabled = YES;
+    };
+    
+    [self.requst start];
 }
 
+- (void)viewDidLoad{
+    //set textfield property
+    self.startLocation.layer.borderWidth = 1;
+    self.startLocation.layer.cornerRadius = 5;
+
+    self.destinationA.layer.borderWidth = 1;
+    self.destinationA.layer.cornerRadius = 5;
+
+    self.destinationB.layer.borderWidth = 1;
+    self.destinationB.layer.cornerRadius = 5;
+
+    self.destinationC.layer.borderWidth = 1;
+    self.destinationC.layer.cornerRadius = 5;
+}
 
 @end
