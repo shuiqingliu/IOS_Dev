5d137b2bc3577da0565588af76badfc17385449f
qingliu <shuiqingliu14@gmail.com>
2017-10-26 08:50:44 +0800

    add:resource of touch controller

--- Assignment2/Assignment2/ViewController.m
+++ Assignment2/Assignment2/ViewController.m
@@ -9,14 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+//statusLab
 @property (weak, nonatomic) IBOutlet UILabel *statusLab;
+//five gesture
+@property (strong, nonatomic) IBOutlet UITapGestureRecognizer *TapGesture;
+@property (strong, nonatomic) IBOutlet UIPinchGestureRecognizer *pinchGesture;
+@property (strong, nonatomic) IBOutlet UIRotationGestureRecognizer *rotaionGesture;
+@property (strong, nonatomic) IBOutlet UIPanGestureRecognizer *panGesture;
+@property (strong, nonatomic) IBOutlet UILongPressGestureRecognizer *longPress;
+@property (weak, nonatomic) IBOutlet UIImageView *monkey;
+@property CGPoint *new;
 
+@property (strong, nonatomic) UIScrollView *sv;
 @end
 
 @implementation ViewController
 
+- (UIScrollView *)scrollView{
+    if(!_sv){
+        _sv = [[UIScrollView alloc] init];
+        CGFloat scrollX = 0;
+        CGFloat scrollY = 112;
+        CGFloat scrollWidth = CGRectGetWidth(self.view.bounds);
+        CGFloat scrollHeight = CGRectGetHeight(self.view.bounds) - 112;
+        _sv.frame = CGRectMake(scrollX, scrollY, scrollWidth, scrollHeight);
+        _sv.backgroundColor = [UIColor blueColor];
+        
+        //_sv.delegate = self;
+        
+        [self.view addSubview:_sv];
+    }
+    return _sv;
+}
+
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     self.statusLab.text = @"屏幕按下";
+    
+    
+    
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
@@ -32,10 +62,29 @@
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     self.statusLab.text = @"触摸取消";
 }
+- (IBAction)tapGesture:(id)sender {
+    
+}
+- (IBAction)pinchGesture:(id)sender {
+}
+
+- (IBAction)Rotaion:(id)sender {
+    
+}
+
+- (IBAction)panGesture:(id)sender {
+    
+    
+}
+- (IBAction)longGesture:(id)sender {
+}
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
