c0acfaee3f21e26cb8f33ab5815bdbd22b58e9d9
qingliu <shuiqingliu14@gmail.com>
2017-11-03 10:55:34 +0800

    completed music play,pause,stop,replay features

--- MusicPlayer/MusicPlayer/ViewController.m
+++ MusicPlayer/MusicPlayer/ViewController.m
@@ -14,24 +14,85 @@
 @property (weak, nonatomic) IBOutlet UIButton *playBtn;
 @property (weak, nonatomic) IBOutlet UIButton *pauseBtn;
 @property (weak, nonatomic) IBOutlet UIButton *muteBtn;
+@property (weak, nonatomic) IBOutlet UIProgressView *musicProgress;
+@property (weak, nonatomic) IBOutlet UILabel *currentTime;
+@property (weak, nonatomic) IBOutlet UILabel *playDuration;
+
+@property  AVAudioPlayer *player;
+@property NSTimer *timer;
 
 @end
 
 @implementation ViewController
 
 - (IBAction)playMusic:(id)sender {
+    [self.player  play];
+    //if timer not valid then restart it
+    if(![self.timer isValid]){
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self
+                                                    selector:@selector(updateTime)
+                                                    userInfo:nil repeats:YES];
+    }
 }
 
 - (IBAction)pauseMusic:(id)sender {
+    //if not playing then start play
+    if(![self.player isPlaying]){
+        [self.player play];
+    }else{
+        [self.player pause];
+    }
+    
 }
 
 - (IBAction)muteMusic:(id)sender {
+    //stop play
+    [self.player stop];
+    self.currentTime.text = @"0.00";
+    //set current progress to zero;
+    self.musicProgress.progress=0.0;
+    self.player.currentTime = 0;
+    //stop timer
+    [self.timer invalidate];
 }
 
+//create music player
+- (void) createPlayer{
+    //get music source
+    NSString *file = [[NSBundle mainBundle] pathForResource:@"now" ofType:@"mp3"];
+    //convert string to URL
+    NSURL *urlOfMusic = [NSURL URLWithString:file];
+    
+    //init audio player
+    self.player = [[AVAudioPlayer alloc]initWithContentsOfURL:urlOfMusic error:nil];
+    //prepare and decode
+    [self.player prepareToPlay];
+    //set player paraments
+    self.player.numberOfLoops = 1;
+    self.player.volume = 0.5;
+    
+    //update progress with timer
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateTime) userInfo:nil repeats:YES];
+    //set play status
+    self.playDuration.text = [NSString stringWithFormat:@"%0.2f",self.player.duration/60];
+    
+    self.player.delegate = self;
+}
+
+- (void)updateTime{
+    self.musicProgress.progress = self.player.currentTime/self.player.duration;
+    //NSNumber *doubleStr = [NSNumber numberWithDouble:self.player.currentTime];
+    self.currentTime.text = [NSString stringWithFormat:@"%.2f",self.player.currentTime/60];
+}
+
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+    [self.timer invalidate];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self createPlayer];
 }
 
 
